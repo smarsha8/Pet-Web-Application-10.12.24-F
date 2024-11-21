34425c1e17d84badb2e3fa517e07839d76389e42
Shakeitha  Marshall <shakeithamarshall@yahoo.com>
2024-11-21 17:00:28 -0600

    Working on the shopping cart

--- Pet Web Application 10.12.24 F/Program.cs
+++ Pet Web Application 10.12.24 F/Program.cs
@@ -5,26 +5,30 @@ using Pet_Web_Application_10._12._24_F.Areas.Data;
 using Pet_Web_Application_10._12._24_F.Data;
 using Microsoft.Extensions.DependencyInjection;
 using System.Configuration;
+using Pet_Web_Application_10._12._24_F.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<PuppiesProductPurchasesDbFContext>(options =>
-
     options.UseSqlServer(builder.Configuration.GetConnectionString("PuppiesProductPurchasesDbFContext") ?? throw new InvalidOperationException("Connection string 'PuppiesProductPurchasesDbFContext' not found.")));
 
 builder.Services.AddDbContext<PuppiesandProductPurchasesContext>(options =>
-
     options.UseSqlServer(builder.Configuration.GetConnectionString("PuppiesandProductPurchasesContext") ?? throw new InvalidOperationException("Connection string 'PuppiesandProductPurchasesContext' not found.")));
 
-// Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
+// Add session and CartService
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<CartService>();
 
 var app = builder.Build();
 
@@ -36,15 +40,15 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession(); // Enable session middleware
 
 app.UseAuthorization();
 
@@ -54,4 +58,3 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
-
